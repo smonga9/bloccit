adb186ebb27ff4914e304cb8df979a04a614895c
Stutay Monga <Stutay.Monga9@gmail.com>
2018-02-01 19:38:21 -0500

    Assignment-24-signing-up partial complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
-  get 'users/create'
+  resources :users, only: [:new, :create]
+  post 'users/confirm' => 'users#confirm'
 
   resources :questions
   resources :topics do
