b326533289d5e29a0246b04cf64b053208be68ac
Stutay Monga <Stutay.Monga9@gmail.com>
2018-02-03 10:20:14 -0500

    Assignment-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,30 @@
 class UsersController < ApplicationController
+  protect_from_forgery
   def new
     @user = User.new
   end
 
   def confirm
      @user = User.new
+     p "confirm ****************"
      @user.name = params[:user][:name]
      @user.email = params[:user][:email]
      @user.password = params[:user][:password]
      @user.password_confirmation = params[:user][:password_confirmation]
+
+     p @user.name
    end
 
   def create
  # #9
      @user = User.new
+     p "create ****************"
      @user.name = params[:user][:name]
      @user.email = params[:user][:email]
      @user.password = params[:user][:password]
      @user.password_confirmation = params[:user][:password_confirmation]
 
+     p @user.name
  # #10
      if @user.save
        flash[:notice] = "Welcome to Bloccit #{@user.name}!"
