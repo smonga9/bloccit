b9b78c55651bb1b4a35bace2d9989fce6d769c89
Stutay Monga <Stutay.Monga9@gmail.com>
2018-03-13 21:50:38 -0400

    Updated Assignment-25

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,10 @@
 module ApplicationHelper
   def form_group_tag(errors, &block)
-    css_class = 'form-group'
-    css_class << ' has-error' if errors.any?
-
-    content_tag :div, capture(&block), class: css_class
+    content_tag :div, capture(&block), class: 'form-group'
   end
 
   def avatar_url(user)
-     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-     "https://s.gravatar.com/avatar/d32d6bc2f4d6a4643bb3f1b24811b0af?s=80"
-   end
-
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://gravater.com/avatar/#{gravatar_id}.png?s=48"
+  end
 end
