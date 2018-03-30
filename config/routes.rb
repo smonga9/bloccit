f265ff1070fee4eeb105fffa55357561ede37751
Stutay Monga <Stutay.Monga9@gmail.com>
2018-03-29 21:37:58 -0400

    Checkpoint 28 Completed

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   resources :posts, except: [:index]
   end
 
+  resources :posts, only: [] do
+
+     resources :comments, only: [:create, :destroy]
+   end
+
   resources :advertisements
   resources :posts
 
