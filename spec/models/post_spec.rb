4f22e6e8d8c251429d072038f8628e8238d63032
Stutay Monga <Stutay.Monga9@gmail.com>
2018-04-01 17:00:37 -0400

    Assignment 29 Completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -64,6 +64,18 @@ RSpec.describe Post, type: :model do
        end
      end
 
+     describe "#create_vote" do
+      it "calls #create_vote when a post is created" do
+         post = topic.posts.new(title: RandomData.random_sentence, body: RandomData.random_sentence, user: user)
+         expect(post).to receive(:create_vote)
+         post.save
+       end
+
+       it "associates the vote with the owner of the post" do
+         expect(post.votes.first.user).to eq(post.user)
+       end
+     end
+
      describe "#update_rank" do
  # #28
        it "calculates the correct rank" do
@@ -76,7 +88,7 @@ RSpec.describe Post, type: :model do
          post.votes.create!(value: 1, user: user)
          expect(post.rank).to eq (old_rank + 1)
        end
- 
+
        it "updates the rank when a down vote is created" do
          old_rank = post.rank
          post.votes.create!(value: -1, user: user)
