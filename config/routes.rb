cfa25f47aba87fa8816c58cabf9ae8619ae9a399
Stutay Monga <Stutay.Monga9@gmail.com>
2018-04-25 17:26:16 -0400

    Checkpoint 31 Completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
-  resource :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
 
   resources :questions
